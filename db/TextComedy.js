eb29208a57171bc37d0253270413175f5763f9ec
Max Volovikov <maxvolovikov@gmail.com>
2018-12-04 00:51:22 -0600

    layout changed a bit more

---
+++ db/TextComedy.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const db = require('./index.js');
+mongoose.Promise = global.Promise;
+
+const TextComedySchema = new mongoose.Schema({
+  rating: Number,  
+  title: String,
+  body: String,
+  author: String,
+  authorImgUrl: String,
+  created: { type: Date, default: Date.now },
+  edited: { type: Date, default: Date.now },
+  reported: Number,
+  punch: String,
+});
+
+const TextComedy = mongoose.model('TextComedy', TextComedySchema);
+
+module.exports = TextComedy;
